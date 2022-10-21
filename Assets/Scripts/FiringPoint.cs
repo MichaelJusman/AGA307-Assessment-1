402de1461a00a7ff908d8a8134d663e4bed16254
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2022-10-21 19:29:29 +1100

    Weapon type

--- Assets/Scripts/FiringPoint.cs
+++ Assets/Scripts/FiringPoint.cs
@@ -8,20 +8,49 @@ public class FiringPoint : MonoBehaviour
     public GameObject projectilePrefab;         //The projectile
     public float projectileSpeed = 1000;        //The speed of projectile
     public Transform firingPoint;               //The point the projectile spawn
+    public LineRenderer laser;
+    public GameObject hitSparks;
+    public GameObject[] WeaponType;
+
+
+   
+
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject projectileInstance = Instantiate(projectilePrefab, firingPoint.position, firingPoint.rotation);
+            FireCannon();
 
-            projectileInstance.GetComponent<Rigidbody>().AddForce(firingPoint.forward * projectileSpeed);
+        }
 
-            Destroy(projectileInstance, 5);
-
+        if (Input.GetButtonDown("Fire2"))
+        {
+            FireRaycast();
         }
     }
 
+    void FireCannon()
+    {
+        GameObject projectileInstance = Instantiate(projectilePrefab, firingPoint.position, firingPoint.rotation);
 
+        projectileInstance.GetComponent<Rigidbody>().AddForce(firingPoint.forward * projectileSpeed);
 
+        Destroy(projectileInstance, 5);
+    }
+
+    void FireRaycast()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, Mathf.Infinity))
+        {
+            laser.SetPosition(0, transform.position);
+            laser.SetPosition(1, hit.point);
+            GameObject party = Instantiate(hitSparks, hit.point, hit.transform.rotation);
+
+                Destroy(party, 2);
+        }
+    }
 }
